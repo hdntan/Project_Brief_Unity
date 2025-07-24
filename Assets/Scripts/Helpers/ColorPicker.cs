77de58f3c8539478a56538c27949d50df73dd5de
hdntan <hdntan0903@gmail.com>
2025-07-24 23:56:48 +0700

    feat: add singleton main manager & set color

--- Assets/Scripts/Helpers/ColorPicker.cs
+++ Assets/Scripts/Helpers/ColorPicker.cs
@@ -3,45 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Script này dùng để tạo một bảng chọn màu cho UI, cho phép người dùng chọn một màu từ danh sách có sẵn.
 public class ColorPicker : MonoBehaviour
 {
+    // Mảng các màu có thể chọn.
     public Color[] AvailableColors;
+    // Prefab nút bấm dùng để hiển thị từng màu.
     public Button ColorButtonPrefab;
     
+    // Màu hiện tại đang được chọn (chỉ đọc từ bên ngoài).
     public Color SelectedColor { get; private set; }
+    // Sự kiện callback khi màu được chọn thay đổi.
     public System.Action<Color> onColorChanged;
 
+    // Danh sách các nút màu đã được tạo ra.
     List<Button> m_ColorButtons = new List<Button>();
     
     // Start is called before the first frame update
+    // Hàm khởi tạo bảng chọn màu, tạo nút cho từng màu trong AvailableColors.
     public void Init()
     {
         foreach (var color in AvailableColors)
         {
+            // Tạo một nút mới từ prefab và gắn vào UI.
             var newButton = Instantiate(ColorButtonPrefab, transform);
+            // Đặt màu cho nút vừa tạo.
             newButton.GetComponent<Image>().color = color;
-            
+
+            // Thêm sự kiện khi nút được bấm.
             newButton.onClick.AddListener(() =>
             {
+                // Cập nhật màu đã chọn.
                 SelectedColor = color;
+                // Đặt tất cả các nút về trạng thái có thể bấm.
                 foreach (var button in m_ColorButtons)
                 {
                     button.interactable = true;
                 }
-
+                // Đặt nút vừa chọn thành không thể bấm (để đánh dấu đang chọn).
                 newButton.interactable = false;
-                
+                // Gọi callback thông báo màu đã thay đổi.
                 onColorChanged.Invoke(SelectedColor);
             });
-            
+
+            // Thêm nút vào danh sách quản lý.
             m_ColorButtons.Add(newButton);
         }
     }
 
+    // Hàm chọn màu theo giá trị Color (dùng cho code, không phải người dùng).
     public void SelectColor(Color color)
     {
         for (int i = 0; i < AvailableColors.Length; ++i)
         {
+            // Nếu màu trùng với màu truyền vào thì gọi sự kiện click của nút đó.
             if (AvailableColors[i] == color)
             {
                 m_ColorButtons[i].onClick.Invoke();
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -13,12 +18,29 @@ public class MenuUIHandler : MonoBehaviour
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
+        Debug.Log("New color selected: " + color);
+        // Set the selected color in MainManager
+        MainManager.Instance.TeamColor = color;
     }
-    
+
     private void Start()
     {
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.ExitPlaymode();
+        #else
+                    Application.Quit();
+        #endif
     }
 }
